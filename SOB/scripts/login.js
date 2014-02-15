bbdeb57a9602473753c2b62207f233fd46f47437
ilink rao <raoilink@gmail.com>
2014-02-15 03:08:57 -0600

    Changes made by ilink rao (raoilink@gmail.com).

--- SOB/scripts/login.js
+++ SOB/scripts/login.js
@@ -1,52 +1,48 @@
-(function (global) {
-    var LoginViewModel,
-        app = global.app = global.app || {};
-
-    LoginViewModel = kendo.data.ObservableObject.extend({
-        isLoggedIn: false,
-        username: "",
-        password: "",
-
-        onLogin: function () {
-            var that = this,
-                username = that.get("username").trim(),
-                password = that.get("password").trim();
-
-            if (username === "" || password === "") {
-                navigator.notification.alert("Both fields are required!",
-                    function () { }, "Login failed", 'OK');
-
-                return;
-            }
-
-            that.set("isLoggedIn", true);
-        },
-
-        onLogout: function () {
-            var that = this;
-
-            that.clearForm();
-            that.set("isLoggedIn", false);
-        },
-
-        clearForm: function () {
-            var that = this;
-
-            that.set("username", "");
-            that.set("password", "");
-        },
-
-        checkEnter: function (e) {
-            var that = this;
-
-            if (e.keyCode == 13) {
-                $(e.target).blur();
-                that.onLogin();
-            }
+var app = new kendo.mobile.Application();
+function loginonMouseOver(){
+    $("#btn_login").css('background', "url(./styles/home/images/loginbtn_click.png)");
+    $("#btn_login").css('background-repeat', "no-repeat");
+    $("#btn_login").css('background-size', "100% 100%");
+}
+function loginonMouseOut(){
+    $("#btn_login").css('background', "url(./styles/home/images/loginbtn.png)");
+    $("#btn_login").css('background-repeat', "no-repeat");
+    $("#btn_login").css('background-size', "100% 100%");
+}
+// 退出应用 
+function exit()
+{
+    navigator.app.exitApp(); 
+}
+/*登录*/
+function login() {
+    var a = $("#userName").val();
+    var b = $("#passWord").val();
+    if (a == "" || a == null) {
+        navigator.notification.alert("小伙伴们，用户名不能为空！", function() {}, "登录提示", "点我继续");
+        return false;
+    }
+    if (b == "" || b == null) {
+        navigator.notification.alert("小伙伴们，请输入密码！", function() {}, "登录提示", "点我继续");
+        return false;
+    }
+    return $.ajax({
+        type: "Post",
+        data:{a:a,b:b},
+        dataType:"html",
+        url:"http://192.168.0.119:8080/SOB/webas/login.do",
+    }).done(function (data) {
+           app.navigate("home.html");
+        /**if (data.result == true) {
+             app.navigate("home.html");
         }
+        else 
+        {
+            navigator.notification.alert(data.content, function() {}, "登录提示", "点我继续");
+            return false;
+        }**/
+    }).fail(function () {
+        navigator.notification.alert("亲，您登录失败，请检查网络是否开启等故障！", function() {}, "登录提示", "明白");
+        return false;
     });
-
-    app.loginService = {
-        viewModel: new LoginViewModel()
-    };
-})(window);
+}
